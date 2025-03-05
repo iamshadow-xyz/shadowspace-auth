1ed9d4e3a9817c60650b4e38dda244eb797b7631
Shadow <iamshadow.xyz@gmail.com>
2025-03-05 15:41:47 +0530

    Add Prisma setup and authentication middleware

--- auth.ts
+++ auth.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/lib/prisma"
 import Google from "next-auth/providers/google"
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Google({
     clientId: process.env.GOOGLE_CLIENT_ID as string,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
